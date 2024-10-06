5dbe3a9eae9a9075e9d06d70940882db02b9adde
Francisco Springer <springerfrancisco@gmail.com>
2024-10-05 22:57:43 -0400

    added comment

--- HW2/My_Website/middleware/logEvents.js
+++ HW2/My_Website/middleware/logEvents.js
@@ -5,15 +5,17 @@ const fs = require('fs');
 const fsPromises = require('fs').promises;
 const path = require('path');
 
+//create a log event with date, time, uuid and message
 const logEvents = async (message, logName) => {
     const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
     const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
 
-    try {
+    try {    
+        //if logs dir does not exist, create dir.
         if (!fs.existsSync(path.join(__dirname, '..', 'logs'))) {
             await fsPromises.mkdir(path.join(__dirname, '..', 'logs'));
         }
-
+        //append will create logName if it does not exist
         await fsPromises.appendFile(path.join(__dirname, '..', 'logs', logName), logItem);
     } catch (err) {
         console.log(err);
